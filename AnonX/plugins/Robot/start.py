09aa03e746d3a998dae844d86970b63e139702d3
niketrawat <144231088+niketrawat@users.noreply.github.com>
2023-09-09 13:28:58 +0530

    Update start.py

--- AnonX/plugins/Robot/start.py
+++ AnonX/plugins/Robot/start.py
@@ -39,7 +39,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("AAMCBQADGQEAAQ8xR2T7Lf8CNaO84k2kfOJS9OUqsCtWAAJECgAC17fBVpxaVdK2DBujAQAHbQADMAQ")
+            await message.reply_sticker("")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
@@ -84,7 +84,7 @@ async def start_comm(client, message: Message, _):
                     details = stats.get(vidid)
                     title = (details["title"][:35]).title()
                     if vidid == "telegram":
-                        msg += f"🔗[ᴛᴇʟᴇɢʀᴀᴍ ᴍᴇᴅɪᴀ](https://t.me/Mr_handsome1234) ** ᴩʟᴀʏᴇᴅ {count} ᴛɪᴍᴇs**\n\n"
+                        msg += f"🔗[ᴛᴇʟᴇɢʀᴀᴍ ᴍᴇᴅɪᴀ](https://t.me/) ** ᴩʟᴀʏᴇᴅ {count} ᴛɪᴍᴇs**\n\n"
                     else:
                         msg += f"🔗 [{title}](https://www.youtube.com/watch?v={vidid}) ** played {count} times**\n\n"
                 msg = _["ustats_2"].format(tot, tota, limit) + msg
@@ -157,10 +157,7 @@ async def start_comm(client, message: Message, _):
                     [
                         InlineKeyboardButton(
                             text="• ʏᴏᴜᴛᴜʙᴇ •", url=f"{link}"
-                        ),
-                        InlineKeyboardButton(
-                            text="• sᴜᴩᴩᴏʀᴛ •", url="https://t.me/chating_masti"
-                        ),
+                        
                     ],
                 ]
             )
@@ -188,7 +185,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("AAMCBQADGQEAAQ8xR2T7Lf8CNaO84k2kfOJS9OUqsCtWAAJECgAC17fBVpxaVdK2DBujAQAHbQADMAQ")
+                await message.reply_sticker("")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
